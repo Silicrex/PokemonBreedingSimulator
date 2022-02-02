d4897444368c54a54d898a75a39ce9b23dfe8d58
Silicrex <silicrex@gmail.com>
2022-02-01 23:51:27 -0500

    Changed inherit_stat.py to inherit_stats.py, made compatible with multiple stats

---
+++ inherit_stats.py
@@ -0,0 +1,18 @@
+from helper_functions import *
+runs = 100000
+
+specific_stats = 1  # Chance of how many specific stats being inherited? Range of 1 through number of IVs inherited
+destiny_knot = True  # True or False toggle
+
+stats_included = 0  # Number of times successful
+for _ in range(runs):
+    iv_slots = [0, 1, 2, 3, 4, 5]
+    iv_slots = roll_inheritance(iv_slots, destiny_knot)
+    # Arbitrarily use first 'specific_stats' stats as target
+    # Check if that set is a subset of the IV slots we're left with
+    if set(range(specific_stats)).issubset(set(iv_slots)):
+        stats_included += 1
+
+result = stats_included / runs
+print(f'Avg % of times specific {specific_stats} inherited: {stats_included / runs:.4%}\n'
+      f'Avg % of times not: {(runs - stats_included) / runs:.4%}')
